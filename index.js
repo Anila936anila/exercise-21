5655efd3aed9bf484708ccc3afbaca8298c72e7d
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:48:19 -0800

    Think of something
    Typescript 45Q assignment exercise 21

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+// Think of something you could store in a Typescript object.
+const bouquet;
+// Define an Array of objects.
+let bouquets = [];
+//pushing multiple object
+bouquets.push(bouquet);
+bouquets.push({
+    names: "Summer bliss",
+    price: 450,
+    descripton: "beautiful arrangement of summer flowers"
+});
+let bouquet3 = {
+    name: "Red Valvet",
+    price: 300,
+    description: "Beautiful arrangement of res lillies"
+};
+bouquets.push(bouquet3);
